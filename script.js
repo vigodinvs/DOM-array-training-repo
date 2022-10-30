43ea02188715c43a3fecfa809edf3b308cff9f90
carter3838 <vigodinvs@gmail.com>
2022-10-30 12:30:26 +0300

    added variables

---
+++ script.js
@@ -0,0 +1,21 @@
+
+//variables
+let addUser = document.getElementById('add-user')
+let doubleMoney = document.getElementById('double')
+let showMillionaires = document.getElementById('show-millionaires')
+let sortByWealth = document.getElementById('sort')
+let calculateWealt = document.getElementById('calculate-wealth')
+let main = document.getElementById('main')
+
+
+
+
+//
+
+
+
+
+
+
+
+
