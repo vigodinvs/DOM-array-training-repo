348a5af21771c2460281c405db13e098673279dd
carter3838 <vigodinvs@gmail.com>
2022-10-30 14:27:46 +0200

    added functions to fetch random users, generate money and push everything into array

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 
 //variables
-let addUser = document.getElementById('add-user')
-let doubleMoney = document.getElementById('double')
-let showMillionaires = document.getElementById('show-millionaires')
-let sortByWealth = document.getElementById('sort')
-let calculateWealt = document.getElementById('calculate-wealth')
+let addUserBtn = document.getElementById('add-user')
+let doubleMoneyBtn = document.getElementById('double')
+let showMillionairesBtn = document.getElementById('show-millionaires')
+let sortByWealthBtn = document.getElementById('sort')
+let calculateWealtBtn = document.getElementById('calculate-wealth')
 let main = document.getElementById('main')
 
+let data = []
 
+// fetch random user, generate amount of money and add it to the object
 
+async function getRandomUser() {
+    let res = await fetch('https://randomuser.me/api');
+    let data = await res.json()
+    let user = data.results[0]
 
-//
+    let  newUser
+ = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000)
+ }    
+ addData(newUser)
+}
 
+// call the function and push three random people to the data array
+
+getRandomUser()
+getRandomUser()
+getRandomUser()
+
+// add new object to data array
+
+function addData (obj) {
+    data.push(obj);
+}
 
 
 
